d0f5d24d3a6334f91fddcd733c0a7785405ddf15
U-Alice <umugwanezaalice22@gmail.com>
2022-05-22 17:20:28 +0200

    added get files routes

--- routes.js
+++ routes.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const router = express.Router()
+const crypto = require('crypto')
 const multer = require('multer')
-const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, './uploads')
-    },
-    filename: (req, file, cb)=>{
-        cb(null, file.originalname )
-    }
+const GridFsStorage = require('multer-gridfs-storage')
+const storage = new GridFsStorage({
+ url: process.env.URL, 
+ file:(req, res)=>{
+     return new Promise((resolve, reject)=>{
+         crypto.randomBytes(16, (err, buf)=>{
+             if(err){
+                 return reject(err)
+             }
+             const filename = buf.toString('hex') + path.extname(file.originalname);
+             const fileinfo = {
+                 filename: filename, 
+                 bucketName: 'uploads'
+             }
+             resolve(fileinfo)
+         })
+     })
+ }
 })
-const upload = multer({storage: storage})
-const {uploadBook} = require('./handlers/handlers')
+const upload = multer({storage})
+const {uploadBook} = require('./handlers/handlers');
+const { GridFsStorage } = require('multer-gridfs-storage');
 
 module.exports.router = (app)=>{
     router.post('/newBook' ,upload.single('book'), uploadBook());
-    // router.get('/uploads',(req, res)=>{
-    //     res.render('index')
-    // })
     app.use(router)
+    router.get('/uploaded', (req, res, next)=>{
+    gsf.find().toArray((err, files)=>{
+        if(!files || files.length === 0 ){
+            return res.status(200).json({
+                success: false, 
+                message: "No files available"
+            })
+        } 
+        files.map(file =>{
+            if(file.contentType === 'image/jpeg' || file.contentType === "image/png" || file.contentType === "image/svg+xml"){
+                file.isImage = true;
+            }else{
+                file.isImage = false;
+            }
+        })
+        res.status(200).json({
+            success: true, files
+        })
+    })
+    })
 }
