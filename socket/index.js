d83663906d41273d8562a747de5ce06a37280606
u-Alice <umugwanezaalice22@gmail.com>
2022-07-11 17:54:17 +0200

    getUsers

--- socket/index.js
+++ socket/index.js
@@ -2,9 +2,17 @@
 const io = require("socket.io")(8900, {cors: {
     origin: "http://localhost:3000"
 }});
+let users = [];
 
+const addUser = (userId , socketId)=>{
+    !users.some(user => user.userId ===  userId) && users.push ({userId , socketId})
+}
 io.on("connection", (socket)=>{
     console.log("A user connected.");
+    socket.on("addUser", (data)=>{
+        addUser(data, socket.id)
+        io.emit("getUsers", users)
+    }) 
 })
 
 
