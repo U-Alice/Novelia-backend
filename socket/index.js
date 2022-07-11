8bbdb7a9096ea4fba778c25b7c56095a2be7f471
u-Alice <umugwanezaalice22@gmail.com>
2022-07-11 17:58:18 +0200

    remove user

--- socket/index.js
+++ socket/index.js
@@ -7,12 +7,19 @@ let users = [];
 const addUser = (userId , socketId)=>{
     !users.some(user => user.userId ===  userId) && users.push ({userId , socketId})
 }
+const removeUser = (socketId)=>{
+users =  users.filter(user => user.socketId !== socketId);
+}
 io.on("connection", (socket)=>{
     console.log("A user connected.");
     socket.on("addUser", (data)=>{
         addUser(data, socket.id)
         io.emit("getUsers", users)
     }) 
+    socket.on("disconnect", ()=>{
+        console.log("user disconnected");
+        removeUser(socket.id)
+    })
 })
 
 
