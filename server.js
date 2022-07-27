8f61cab5e9f4f98799ad481f382e2f8f5817bc9c
u.Alice <umugwanezaalice22@gmail.com>
2022-07-27 16:26:09 +0200

    added allow cross origin header

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use((req,res, next)=>{
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next()
 })
 router(app);
